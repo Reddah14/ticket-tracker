ecf82b7d0dd806267eb159d093450177c6c76f09
Angaar Uriakhil <angar1996@gmail.com>
2021-09-15 09:13:59 +0100

    updated a generic class name to something more specific

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -19,14 +19,14 @@ const Ticket = (props) => {
 
   return (
     <div className="ticket">
-      <h2 className="ticket__name ticket__generic">{employeeName}</h2>
-      <h2 className="ticket__role ticket__generic">{employeeRole}</h2>
-      <hr className="ticket__line ticket__generic"/> 
-      <div className="ticket__counter ticket__generic">
-        <h3 className = "ticket__counter-label ticket__generic">Counter</h3>
-        <h3 className = "ticket__counter-number ticket__generic">{counter}</h3>
-        <FaMinus onClick = {handleDecrement} className = "ticket__minus-button ticket__generic"/>
-        <FaPlus onClick = {handleIncrement} className = "ticket__plus-button ticket__generic"/> 
+      <h2 className="ticket__name ticket__sub-item">{employeeName}</h2>
+      <h2 className="ticket__role ticket__sub-item">{employeeRole}</h2>
+      <hr className="ticket__line ticket__sub-item"/> 
+      <div className="ticket__counter ticket__sub-item">
+        <h3 className = "ticket__counter-label ticket__sub-item">Counter</h3>
+        <h3 className = "ticket__counter-number ticket__sub-item">{counter}</h3>
+        <FaMinus onClick = {handleDecrement} className = "ticket__minus-button ticket__sub-item"/>
+        <FaPlus onClick = {handleIncrement} className = "ticket__plus-button ticket__sub-item"/> 
       </div>
     </div>
   );
